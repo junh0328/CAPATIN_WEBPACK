b017ac6cec4359d6c8d2d77232f13e200bfc76b6
이준희 <junh0328@naver.com>
2021-09-12 15:56:13 +0900

    실전 적용하기

--- ex.js
+++ ex.js
@@ -1,2 +1,18 @@
+// map > 배열로 결과값을 반환한다.
+
+// 가능 (배열)
 const result = [1, 2, 3].map((v) => v ** 2);
 console.log(result);
+
+// 가능 (값)
+[1, 2, 3].map((v) => console.log(v ** 2));
+
+// forEach > 배열로 결과값을 반환하지 않는다.
+// 따라서 forEach 함수 내부에서 결과를 따로 반환해줘야 한다
+
+// 불가능 (배열)
+const result2 = [1, 2, 3].forEach((v) => v ** 2);
+console.log(result2);
+
+// 가능 (값)
+[1, 2, 3].forEach((v) => console.log(v ** 2));
--- exercise/webpack.config.js
+++ exercise/webpack.config.js
@@ -3,19 +3,24 @@ var webpack = require("webpack");
 
 module.exports = {
   mode: "production",
+  // (웹팩 대상 파일의 경로)루트 디렉토리에 main.js를 번들링한다
   entry: "./src/main.js",
+  // 번들링된 output은 루트 디렉토리에 dist 폴더를 만들어 build.js라는 파일에 넣는다.
   output: {
     path: path.resolve(__dirname, "./dist"),
     publicPath: "/dist/",
     filename: "build.js",
   },
+
   module: {
     rules: [
       {
+        // .css 확장자를 가진 파일 또한 웹팩에서 인식할 수 있도록 css-loader 로 읽고, vue-style-loader를 적용한다
         test: /\.css$/,
         use: ["vue-style-loader", "css-loader"],
       },
       {
+        // .vue 확장자를 가진 파일은 vue-loader를 통해 따로 분리한다.
         test: /\.vue$/,
         loader: "vue-loader",
         options: {
@@ -24,11 +29,14 @@ module.exports = {
         },
       },
       {
+        // .js 확장자를 가진 파일은 바벨로더를 적용한다
+        // 바벨은 높은 버전의 문법을 범용성을 최대한 갖춘 상태로 낮춰주는 용도이다
         test: /\.js$/,
         loader: "babel-loader",
         exclude: /node_modules/,
       },
       {
+        // 웹팩에서 .png/.jpg/.gif/.svg 파일을 인식할 수 있도록 파일 로더를 적용한다
         test: /\.(png|jpg|gif|svg)$/,
         loader: "file-loader",
         options: {
@@ -37,22 +45,33 @@ module.exports = {
       },
     ],
   },
+  // 웹팩으로 파일을 해석할 때 해석방식을 정의하는 것이다.
   resolve: {
     alias: {
       vue$: "vue/dist/vue.esm.js",
     },
     extensions: ["*", ".js", ".vue", ".json"],
   },
+  // ?
   devServer: {
     historyApiFallback: true,
     noInfo: true,
     overlay: true,
   },
+
+  // ?
   performance: {
     hints: false,
   },
+
+  // ?
   devtool: "#eval-source-map",
 };
+
+/* 
+webpack 버전 3까지는 해당 내용처럼 따로 production 모드일 때 에 대한 추가 설정을 해줘야 했지만,
+webpack 버전 4부터는 mode: "production", 등으로 간단하게 나타낼 수 있게 되었다.
+*/
 
 // if (process.env.NODE_ENV === 'production') {
 //   module.exports.devtool = '#source-map'
